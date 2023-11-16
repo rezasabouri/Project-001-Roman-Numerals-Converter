a4f07fa2e92d89435b1cfee387e73db46a7aa0f8
reza <sabouri.re@gmail.com>
2023-11-16 20:19:18 +0100

    removed debug

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def main_post():
         return render_template('index.html', developer_name='Reza', not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
 
 
